7bb2955e1d5a511879fa11557eddd3bb21449cd3
noothanpanthayi <noothankrishnan@outlook.com>
2025-09-21 13:24:17 -0500

    all update

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,12 +1,20 @@
 import { Pool } from 'pg'
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: 5432
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Neon
+  },
 });
+
 
 interface Station {
   id: number
